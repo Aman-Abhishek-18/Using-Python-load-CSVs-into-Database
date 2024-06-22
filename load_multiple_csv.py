ffb468b1ff0addb45d08044acdb3b80b701745d8
Aman Abhishek <70442464+Aman-Abhishek-18@users.noreply.github.com>
2024-06-22 15:04:39 +0530

    Update load_multiple_csv.py

--- load_multiple_csv.py
+++ load_multiple_csv.py
@@ -3,21 +3,34 @@ from sqlalchemy import create_engine
 
 # Define your connection string with the ODBC driver name
 conn_string = (
-    "mssql+pyodbc://sa:test123@localhost/SQLCaseStudyDB"
+    "mssql+pyodbc://username:password@localhost/databaseName"
     "?driver=ODBC+Driver+17+for+SQL+Server"
 )
+
 try:
     # Create an engine
     engine = create_engine(conn_string)
-    
-    # Load your CSV file
-    df = pd.read_csv(r'C:\Users\amana\OneDrive\Desktop\Python-MS SQL Server\SQL Case Study - Datasets\artist.csv')
 
-    # Example operation: Writing data to SQL Server
-    df.to_sql('artist', con=engine, if_exists='replace', index=False)
+    # List of files to load
+    files = ['artist.csv', 'canvas_size.csv', 'image_link.csv', 
+             'museum_hours.csv', 'museum.csv', 'product_size.csv', 
+             'subject.csv', 'work.csv']
 
-    print("Data loaded successfully.")
-    print(df.info)
+    # Iterate through each file
+    for file in files:
+        # Construct the full path to each CSV file
+        file_path = fr'C:\Users\amana\OneDrive\Desktop\Python-MS SQL Server\SQL Case Study - Datasets\{file}'
+
+        # Load the CSV file into a DataFrame
+        df = pd.read_csv(file_path)
+
+        # Extract table name from file name (remove .csv extension)
+        table_name = file.split('.')[0]
+
+        # Write data to SQL Server
+        df.to_sql(table_name, con=engine, if_exists='replace', index=False)
+
+        print(f"Data from {file} loaded successfully.")
 
 except Exception as e:
     print(f"An error occurred: {str(e)}")
